0ef5538a6bdaa21a00b485006e89386a404cd6e7
Alice Barbe <alice_barbe@hotmail.com>
2022-02-13 08:59:58 -0500

    add list of users

--- twilio_notif.py
+++ twilio_notif.py
@@ -7,18 +7,30 @@ Created on Sat Feb 12 11:37:20 2022
 
 import json
 from twilio.rest import Client
+from pymongo import MongoClient
+import certifi
+import pandas as pd
 
 with open('credentials.json', 'r') as f:
     creds = json.load(f)
 
+# get numbers of users from database
+mongostr = creds["mongostr"]
+client = MongoClient(mongostr, tlsCAFile=certifi.where())
+
+db = client["shoestring"]
+collection = db.notifnumbers
+notifnumbers = pd.DataFrame(list(collection.find()))
+
+# send text messages
 client = Client(creds["twilio"]["account_sid"], creds["twilio"]["auth_token"])
 
 def send_sms(number, message):
-    message = client.messages \
-        .create(
-            messaging_service_sid='MGc698655e624c15d697502e522479501b',
-            body=message,
-            to=number
-         )
+    message = client.messages.create(
+        messaging_service_sid='MGc698655e624c15d697502e522479501b',
+        body=message,
+        to=number)
 
-    print(message.sid)
+def send_sms_to_all(message):
+    for index, row in notifnumbers.iterrows():
+        send_sms(row['number'], f"Hi {row['name']}, {message}")
